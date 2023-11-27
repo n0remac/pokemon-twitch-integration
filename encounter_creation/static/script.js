6e8538c499820a80d04908821f64058d9c9fde34
Cameron Smart <smartcameron@gmail.com>
2023-11-27 06:13:15 -0800

    map squares stay clicked

--- encounter_creation/static/script.js
+++ encounter_creation/static/script.js
@@ -1,23 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const imageContainer = document.querySelector('.image-container');
-    const hoverBox = document.createElement('div');
-    hoverBox.classList.add('hover-box', 'hidden');
-    imageContainer.appendChild(hoverBox);
+    const hoverBox = createBox('green'); // Box for hover
+    let selectedBox = null; // Box for the selected tile
 
     const tileSize = 48;
 
+    function createBox(color) {
+        const box = document.createElement('div');
+        box.classList.add('hover-box', 'hidden');
+        box.style.borderColor = color;
+        imageContainer.appendChild(box);
+        return box;
+    }
+
     imageContainer.addEventListener('mousemove', function(e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        // Calculate grid snap positions
-        const gridX = Math.floor(x / tileSize) * tileSize;
-        const gridY = Math.floor(y / tileSize) * tileSize;
-
-        // Position the hover box
-        hoverBox.style.left = gridX + 'px';
-        hoverBox.style.top = gridY + 'px';
+        positionBox(e, hoverBox);
         hoverBox.classList.remove('hidden');
     });
 
@@ -25,25 +22,61 @@ document.addEventListener('DOMContentLoaded', function() {
         hoverBox.classList.add('hidden');
     });
 
-
     imageContainer.addEventListener('click', function(e) {
-        const rect = this.getBoundingClientRect();
+        // If clicking on the existing selected box, remove it
+        if (selectedBox && !selectedBox.classList.contains('hidden') &&
+            selectedBox.style.left === hoverBox.style.left &&
+            selectedBox.style.top === hoverBox.style.top) {
+            selectedBox.remove();
+            selectedBox = null;
+            updateCoordinatesField('');
+            return;
+        }
+
+        // Create or reposition the selected box
+        if (!selectedBox) {
+            selectedBox = createBox('black');
+        } else {
+            selectedBox.classList.remove('hidden');
+        }
+        positionBox(e, selectedBox);
+
+        // Update the form with the selected coordinates
+        const coordinates = getCoordinates(e);
+        updateCoordinatesField(coordinates.x + ',' + coordinates.y);
+    });
+
+    function positionBox(e, box) {
+        const rect = imageContainer.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-    
+
         // Calculate grid snap positions
-        const gridX = Math.floor(x / tileSize);
-        const gridY = Math.floor(y / tileSize);
-    
-        // Update the form with the selected coordinates
+        const gridX = Math.floor(x / tileSize) * tileSize;
+        const gridY = Math.floor(y / tileSize) * tileSize;
+
+        // Position the box
+        box.style.left = gridX + 'px';
+        box.style.top = gridY + 'px';
+    }
+
+    function getCoordinates(e) {
+        const rect = imageContainer.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        return {
+            x: Math.floor(x / tileSize),
+            y: Math.floor(y / tileSize)
+        };
+    }
+
+    function updateCoordinatesField(value) {
         const coordinatesField = document.getElementById('id_coordinates');
         if (coordinatesField) {
-            coordinatesField.value = gridX + ',' + gridY;
+            coordinatesField.value = value;
         } else {
             console.error("Coordinates field not found");
         }
-    });
-
-    
-
+    }
 });
